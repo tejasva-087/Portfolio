bacd4e36b21f247a53928bee16066aedb75ebf8b
Tejasva <khandelwaltejasva@gmail.com>
2024-02-18 00:59:50 +0530

    Commiting to github

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const navigation = document.querySelector(".nav");
+const heroSection = document.querySelector(".hero-section");
+const navPosition = navigation.getBoundingClientRect();

963b482c7d0c3ed6e00c3330a979aeca7e463b58
tejasva-087 <khandelwaltejasva@gmail.com>
2024-04-11 23:04:58 +0530

    DOne with the about section

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-"use strict";
+// Code to automatically insert HTML for tech stack
 
-const navigation = document.querySelector(".nav");
-const heroSection = document.querySelector(".hero-section");
-const navPosition = navigation.getBoundingClientRect();
+// HTML ELEMENTS
+const techIconBox = document.querySelector(".tech-icon-box");
+
+// Fetching data from the json file and making the changes accordingly
+fetch("./data.json")
+  .then((res) => res.json())
+  .then((data) => {
+    // Inserting the techStack Data
+    data.techStack.forEach((val) => {
+      techIconBox.insertAdjacentHTML(
+        "afterbegin",
+        `<img src="/assets/icons/${val}.png" alt="${val} icon" class="icon-mid-bg" />`
+      );
+    });
+  });

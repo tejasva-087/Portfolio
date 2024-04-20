1602a3c98f873243f1320c91e0a54ceb27b78818
tejasva-087 <khandelwaltejasva@gmail.com>
2024-04-21 03:10:37 +0530

    Made the tabbed component and implemented its functionality

--- script.js
+++ script.js
@@ -38,6 +38,41 @@ const data = {
       link: "https://drive.google.com/file/d/13ZYQyw4csshy7nouL9haH1vzzlD0x9Mk/view?usp=sharing",
     },
   ],
+
+  experience: [
+    {
+      image: "firefox",
+      name: "Mozilla Firefox Club",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloremque aut autem minus maiores nam pariatur deserunt, ad libero nihil, impedit rem aperiam iure voluptatem enim consectetur? Fuga, assumenda ab.",
+      linkText: "View site",
+      link: "https://github.com/tejasva-087/codex_tejasva-khandelwal-70.17.159.182",
+    },
+    {
+      image: "healthotech",
+      name: "Health-O-Tech",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloremque aut autem minus maiores nam pariatur deserunt, ad libero nihil, impedit rem aperiam iure voluptatem enim consectetur? Fuga, assumenda ab.",
+      linkText: "View site",
+      link: "https://github.com/tejasva-087/codex_tejasva-khandelwal-70.17.159.182",
+    },
+    {
+      image: "codex",
+      name: "Code-X",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloremque aut autem minus maiores nam pariatur deserunt, ad libero nihil, impedit rem aperiam iure voluptatem enim consectetur? Fuga, assumenda ab.",
+      linkText: "View site",
+      link: "https://github.com/tejasva-087/codex_tejasva-khandelwal-70.17.159.182",
+    },
+    {
+      image: "halocon",
+      name: "Halocon 4.0",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo doloremque aut autem minus maiores nam pariatur deserunt, ad libero nihil, impedit rem aperiam iure voluptatem enim consectetur? Fuga, assumenda ab.",
+      linkText: "View site",
+      link: "https://github.com/tejasva-087/codex_tejasva-khandelwal-70.17.159.182",
+    },
+  ],
 };
 
 ///////////////////////////////
@@ -124,3 +159,80 @@ const slideLeft = function () {
 
 sliderBtnRight.addEventListener("click", slideRight);
 sliderBtnLeft.addEventListener("click", slideLeft);
+
+//////////////////////////////////
+// CREATING TABBED COMPONENT HTML
+//////////////////////////////////
+
+// HTML ELEMENTS
+const btnContainer = document.querySelector(".tab-comp-btn-container");
+const dataContainer = document.querySelector(".tab-comp-data-container");
+
+// CREATING TAB BTN COMPONENT
+data.experience.forEach((val, i) => {
+  btnContainer.insertAdjacentHTML(
+    "beforeend",
+    `<button class="tab-comp-btn tab-comp-btn${
+      i === 0 ? "--active" : ""
+    } text-sml" data-btn-no="${i}">
+    ${val.name}
+  </button>`
+  );
+});
+// CREATING TAB DATA COMPONENT
+data.experience.forEach((val, i) => {
+  dataContainer.insertAdjacentHTML(
+    "beforeend",
+    `<div class="tab-comp-data tab-comp-data${
+      i === 0 ? "--active" : ""
+    }" id="temp-comp-data--${i}">
+    <img
+    src="./assets/experience/${val.image}.png"
+    alt="${val.image} logo"
+    class="tab-comp-img margin-bottom-sml-2"
+  />
+  <h3
+    class="text-mid-light text-primary-light-1 margin margin-bottom-sml-2"
+  >
+    ${val.name}
+  </h3>
+  <p class="text-mid text-primary-light-1 margin-bottom-sml-2">
+    ${val.description}
+  </p>
+  <div class="text-sml text-primary-white-1">
+    <a href="${val.link}" class="link link-white" target="_blank">
+      <div class="flex-center">
+        <span>${val.linkText}</span>
+        <i class="ph ph-arrow-right"></i>
+      </div>
+    </a>
+  </div>
+  </div>`
+  );
+});
+
+//////////////////////////////////
+// TABBED COMPONENT fUNCTIONALITY
+//////////////////////////////////
+
+// Implement event deligation
+btnContainer.addEventListener("click", (e) => {
+  // Cheking for the element that triggered event
+  if (e.target.classList.contains("tab-comp-btn")) {
+    // Removing the active class from all the elements
+    btnContainer
+      .querySelector(".tab-comp-btn--active")
+      .classList.remove("tab-comp-btn--active");
+    dataContainer
+      .querySelector(".tab-comp-data--active")
+      .classList.remove("tab-comp-data--active");
+
+    // Adding active class to the target component
+    e.target.classList.add("tab-comp-btn--active");
+    console.log(
+      document
+        .getElementById(`temp-comp-data--${e.target.dataset.btnNo}`)
+        .classList.add("tab-comp-data--active")
+    );
+  }
+});
